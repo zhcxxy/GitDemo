bdd1bfa8f0f7cddd11ee70483e3c27585b090e1c
gitDemo <3396899309@qq.com>
2017-09-12 15:24:14 +0800

    提交

---
+++ app/src/main/java/com/jiyun/gitdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //跑哦啊大门口
+    }
+}
